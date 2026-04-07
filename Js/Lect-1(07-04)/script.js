3482249e263939a10d56a80a8cdc03f15739bdc1
megha <meghapatel1.tops@gmail.com>
2026-04-07 12:34:49 +0530

    js start

---
+++ Js/Lect-1(07-04)/script.js
@@ -0,0 +1,23 @@
+let x=100;
+console.log(x);
+
+x=90;
+console.log(x);
+
+const pi=3.14;
+console.log(pi);
+
+//pi=2.7=>gen Error cant reassihn value of constant
+
+var m=170;
+console.log(m);
+
+var m=20;
+console.log(m);
+            
+//             let      const       var
+// Redeclare   no        no         yes
+// ReAssign    yes       no         yes
+
+
+
