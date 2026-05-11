8c92119821d571b1e4b2a119a920bf500188e5be
megha <meghapatel1.tops@gmail.com>
2026-05-11 13:45:04 +0530

    all

--- Js/project/script.js
+++ Js/project/script.js
@@ -1,10 +1,38 @@
 console.log('script loaded');
-import {printCategory} from './Category.js'
-import {printProduct} from './Product.js'
+import { printCategory } from './Category.js'
+import { printProduct } from './Product.js'
 
-function loadPage (){
+ window.logout =function (){
+       let loggedUser = localStorage.getItem('loggedUser');
+            if (loggedUser) {
+                        localStorage.removeItem('loggedUser');
+                        window.location = 'index.html'
+            }
+}
+
+function CheckAuth() {
+      let loggedUser = localStorage.getItem('loggedUser');
+      console.log(loggedUser);
+      let str;
+      if (loggedUser) {
+            loggedUser = JSON.parse(loggedUser);
+            str = `
+                  <span style="color: white;">Welcome:${loggedUser.username.toUpperCase()}</span>
+          <button class="nav-link" onclick = "logout()">Logout</button>
+            `
+            
+      }
+      else {
+            str = `  <a class="nav-link" href="Login.html">Login</a>`
+
+      }
+      document.getElementById('authnavitem').innerHTML = str
+}
+
+function loadPage() {
       printCategory();
       printProduct();
+      CheckAuth();
 }
 
 window.onload = loadPage;
